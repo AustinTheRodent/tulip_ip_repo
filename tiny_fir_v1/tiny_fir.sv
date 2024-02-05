e63c2b7e232b124f8a050f7adb7c11c91f344c95
AustinTheRodent <89956165+AustinTheRodent@users.noreply.github.com>
2024-02-05 17:35:30 -0600

    Update tiny_fir.sv

--- tiny_fir_v1/tiny_fir.sv
+++ tiny_fir_v1/tiny_fir.sv
@@ -38,6 +38,15 @@ module tiny_fir
 
   logic taps_bram_wr_accept;
 
+  logic unsigned [$clog2(G_NUM_TAPS)-1:0] din_bram_start_addr;
+  logic unsigned [$clog2(G_NUM_TAPS)-1:0] din_bram_wr_din_addr;
+  logic [G_DATA_WIDTH-1:0]                din_bram_wr_din_value;
+  logic                                   din_bram_wr_din_valid;
+  logic unsigned [$clog2(G_NUM_TAPS)-1:0] din_bram_rd_din_addr;
+  logic                                   din_bram_rd_din_valid;
+  logic [G_DATA_WIDTH-1:0]                din_bram_rd_dout_value;
+  logic                                   din_bram_rd_dout_valid;
+
 //////////////////////////////////////////////////////
 
   always @ (posedge clk) begin
@@ -52,6 +61,9 @@ module tiny_fir
 
         SM_INIT : begin
           prog_tap_address <= 0;
+          din_bram_wr_din_addr <= 0;
+          din_bram_wr_din_value <= 0;
+          din_bram_wr_din_valid <= 1;
           tap_din_ready <= 1;
           state <= SM_PROGRAM_TAPS;
         end
@@ -59,14 +71,26 @@ module tiny_fir
         SM_PROGRAM_TAPS : begin
           if (tap_din_valid == 1 && tap_din_ready == 1) begin
             if (prog_tap_address == G_NUM_TAPS-1) begin
-              
+              din_ready <= 1;
+              din_bram_start_addr <= 0;
+              din_bram_wr_din_valid <= 0;
+              tap_din_ready <= 0;
+              tap_din_done <= 1;
+              state <= SM_GET_INPUT;
             end
             else begin
+              prog_tap_address     <= prog_tap_address + 1;
+              din_bram_wr_din_addr <= din_bram_wr_din_addr + 1;
             end
           end
         end
 
-        defailt : begin
+        SM_GET_INPUT : begin
+          if (din_valid == 1 && din_ready == 1) begin
+          end
+        end
+
+        default : begin
         end
 
       endcase
@@ -95,6 +119,25 @@ module tiny_fir
     .rd_dout_valid  ()
   );
 
+  tiny_fir_bram
+  #(
+    .G_ADDR_WIDTH  ($clog2(G_NUM_TAPS)),
+    .G_DATA_WIDTH  (G_TAP_WIDTH)
+  )
+  u_din_bram
+  (
+    .clk            (clk),
+  
+    .wr_din_addr    (),
+    .wr_din_value   (),
+    .wr_din_valid   (),
+  
+    .rd_din_addr    (),
+    .rd_din_valid   (),
+  
+    .rd_dout_value  (),
+    .rd_dout_valid  ()
+  );
 
 endmodule
 
