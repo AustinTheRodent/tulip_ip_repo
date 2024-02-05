3d171abac1d12c35edd41a5368f4d9efe3b328b0
AustinTheRodent <89956165+AustinTheRodent@users.noreply.github.com>
2024-02-05 17:19:03 -0600

    Update tiny_fir.sv

--- tiny_fir_v1/tiny_fir.sv
+++ tiny_fir_v1/tiny_fir.sv
@@ -22,3 +22,83 @@ module tiny_fir
     output logic                     dout_valid,
     input  logic                     dout_ready
   );
+
+  typedef enum
+  {
+    SM_INIT,
+    SM_GET_INPUT,
+    SM_CALC_MULT,
+    SM_ACCUMULATE,
+    SM_SEND_OUTPUT
+  } state_t;
+
+//////////////////////////////////////////////////////
+
+  always @ (posedge clk) begin
+    if (reset == 1 || enable == 0) begin
+    end
+    else begin
+    end
+  end
+  
+  tiny_fir_bram
+  #(
+    .G_ADDR_WIDTH  ($clog2(G_NUM_TAPS)),
+    .G_DATA_WIDTH  (G_TAP_WIDTH)
+  )
+  u_taps_bram
+  (
+    .clk            (clk),
+  
+    .wr_din_addr    (),
+    .wr_din_value   (),
+    .wr_din_valid   (),
+  
+    .rd_din_addr    (),
+    .rd_din_valid   (),
+  
+    .rd_dout_value  (),
+    .rd_dout_valid  ()
+  );
+
+
+endmodule
+
+module tiny_fir_bram
+#(
+  parameter int G_ADDR_WIDTH,
+  parameter int G_DATA_WIDTH
+)
+(
+  input logic                     clk,
+
+  input logic [G_ADDR_WIDTH-1:0]  wr_din_addr,
+  input logic [G_DATA_WIDTH-1:0]  wr_din_value,
+  input logic                     wr_din_valid,
+
+  input logic [G_ADDR_WIDTH-1:0]  rd_din_addr,
+  input logic                     rd_din_valid,
+
+  output logic [G_DATA_WIDTH-1:0] rd_dout_value,
+  output logic                    rd_dout_valid
+);
+
+  logic [G_DATA_WIDTH-1:0] bram_memory [0:2**G_ADDR_WIDTH-1];
+
+  always @ (posedge clk) begin
+    if (wr_din_valid == 1) begin
+      bram_memory[wr_din_addr] <= wr_din_value;
+    end
+  end
+
+  always @ (posedge clk) begin
+    rd_dout_value <= bram_memory[rd_din_addr];
+  end
+
+  always @ (posedge clk) begin
+    rd_dout_valid <= rd_din_valid;
+  end
+
+endmodule
+
+
