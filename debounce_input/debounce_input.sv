15313df88780922f9913a2e65688be421c7879f6
AustinTheRodent <89956165+AustinTheRodent@users.noreply.github.com>
2024-02-22 18:14:34 -0600

    Update debounce_input.sv

--- debounce_input/debounce_input.sv
+++ debounce_input/debounce_input.sv
@@ -1,9 +1,22 @@
 module debounce_input
 #(
+  parameter int G_POST_RISING_EDGE_DELAY = 256, // clock cycles to wait after detecting a rising edge before allowing another edge detection
+  parameter int G_POST_FALLING_EDGE_DELAY = 256, // clock cycles to wait after detecting a falling edge before allowing another edge detection
+  parameter int G_RISING_EDGE_MIN_COUNT = 16, // must see G_RISING_EDGE_MIN_COUNT samples of the same value to do a rising edge transition
+  parameter int G_FALLING_EDGE_MIN_COUNT = 16 // must see G_FALLING_EDGE_MIN_COUNT samples of the same value to do a falling edge transition
 )
 (
+  input  logic clk,
+  input  logic aresetn,
+
+  input  logic din_bounce,
+  output logic dout_debounced
+
 );
 
-
+  typedef enum
+  {
+  } state_t;
+  state_t state;
 
 endmodule
