c555f8d57c9c159d032e8c163c27cff037a7b7bd
AustinTheRodent <austincfite@gmail.com>
2024-08-22 20:29:14 -0500

    Add AXI DMA Support.

--- axi_stream_sniffer/axi_stream_sniffer.vhd
+++ axi_stream_sniffer/axi_stream_sniffer.vhd
@@ -52,7 +52,33 @@ end entity;
 
 architecture rtl of axi_stream_sniffer is
 
+  signal registers          : reg_t;
+
+  signal fifo_din           : std_logic_vector(G_DATA_WIDTH-1 downto 0);
+  signal fifo_din_valid     : std_logic;
+  signal fifo_din_ready     : std_logic;
+  signal fifo_dout          : std_logic_vector(G_DATA_WIDTH-1 downto 0);
+  signal fifo_dout_valid    : std_logic;
+  signal fifo_dout_ready    : std_logic;
+  signal fifo_used          : std_logic_vector(G_FIFO_ADDR_WIDTH-1 downto 0);
+  signal fifo_overflow      : std_logic;
+
+  signal transaction_count  : unsigned(31 downto 0);
+
 begin
+
+  p_transaction_count : process(s_axi_aclk)
+  begin
+    if rising_edge(s_axi_aclk) then
+      if s_axi_aresetn = '0' or registers.CONTROL.SW_RESETN(0) = '0' then
+        transaction_count <= (others => '0');
+      else
+        if s_axis_sniff_tvalid = '1' and m_axis_sniff_tready = '1' then
+          transaction_count <= transaction_count + 1;
+        end if;
+      end if;
+    end if;
+  end process;
 
   m_axis_sniff_tdata   <= s_axis_sniff_tdata;
   m_axis_sniff_tvalid  <= s_axis_sniff_tvalid;
@@ -65,17 +91,20 @@ begin
       s_axi_aclk    => s_axi_aclk,
       s_axi_aresetn => s_axi_aresetn,
 
-      s_TRANSACTION_COUNT_COUNT               => (others => '0'),
-      s_TRANSACTION_COUNT_COUNT_v             => '0',
+      s_GLOBAL_STATUS_FIFO_DOUT_VALID(0)      => fifo_dout_valid,
+      s_GLOBAL_STATUS_FIFO_DOUT_VALID_v       => '1',
 
-      s_TRANSACTION_VALUE_DATA                => (others => '0'),
-      s_TRANSACTION_VALUE_DATA_v              => '0',
+      s_TRANSACTION_COUNT_COUNT               => std_logic_vector(transaction_count),
+      s_TRANSACTION_COUNT_COUNT_v             => '1',
+
+      s_TRANSACTION_VALUE_DATA                => fifo_dout,
+      s_TRANSACTION_VALUE_DATA_v              => fifo_dout_valid,
 
       s_GLOBAL_STATUS_TRANSACTION_OVERFLOW    => (others => '0'),
       s_GLOBAL_STATUS_TRANSACTION_OVERFLOW_v  => '0',
 
-      s_FIFO_STATUS_FIFO_AVAILABLE            => (others => '0'),
-      s_FIFO_STATUS_FIFO_AVAILABLE_v          => '0',
+      s_FIFO_STATUS_FIFO_USED                 => std_logic_vector(resize(unsigned(fifo_used), 32)),
+      s_FIFO_STATUS_FIFO_USED_v               => '1',
 
 
       s_axi_awaddr  => s_axi_awaddr,
@@ -100,34 +129,52 @@ begin
       s_axi_rvalid  => s_axi_rvalid,
       s_axi_rready  => s_axi_rready,
 
-      registers_out => open
+      registers_out => registers
     );
 
-    u_ps_2_i2s_fifo : entity work.axis_sync_fifo
-      generic map
-      (
-        G_ADDR_WIDTH    => C_PS_2_I2S_FIFO_AWIDTH,
-        G_DATA_WIDTH    => 2*C_ADC_RESOLUTION,
-        G_BUFFER_INPUT  => true,
-        G_BUFFER_OUTPUT => true
-      )
-      port map
-      (
-        clk             => 
-        reset           => 
-        enable          => 
+  fifo_din        <= s_axis_sniff_tdata;
+  fifo_din_valid  <= s_axis_sniff_tvalid and m_axis_sniff_tready;
 
-        din             => 
-        din_valid       => 
-        din_ready       => 
-        din_last        => '0',
+  p_fifo_overflow : process(s_axi_aclk)
+  begin
+    if rising_edge(s_axi_aclk) then
+      if s_axi_aresetn = '0' or registers.CONTROL.SW_RESETN(0) = '0' then
+        fifo_overflow <= '0';
+      else
+        if fifo_din_valid = '1' and fifo_din_ready = '0' then
+          fifo_overflow <= '1';
+        end if;
+      end if;
+    end if;
+  end process;
 
-        used            => 
+  u_ps_2_i2s_fifo : entity work.axis_sync_fifo
+    generic map
+    (
+      G_ADDR_WIDTH    => G_FIFO_ADDR_WIDTH,
+      G_DATA_WIDTH    => G_DATA_WIDTH,
+      G_BUFFER_INPUT  => true,
+      G_BUFFER_OUTPUT => true
+    )
+    port map
+    (
+      clk             => s_axi_aclk,
+      reset           => (not s_axi_aresetn),
+      enable          => registers.CONTROL.SW_RESETN(0),
 
-        dout            => 
-        dout_valid      => 
-        dout_ready      => 
-        dout_last       => 
-      );
+      din             => fifo_din,
+      din_valid       => fifo_din_valid,
+      din_ready       => fifo_din_ready,
+      din_last        => '0',
+
+      used            => fifo_used,
+
+      dout            => fifo_dout,
+      dout_valid      => fifo_dout_valid,
+      dout_ready      => fifo_dout_ready,
+      dout_last       => open
+    );
+
+    fifo_dout_ready <= registers.TRANSACTION_VALUE_REG_rd_pulse;
 
 end rtl;
